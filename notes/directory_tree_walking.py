b2deddb22b21abe8ed7489ef883e01642298781a
Christy Simmons <cmorrisond@gmail.com>
2020-02-24 13:49:27 -0800

    Update directory_tree_walking.py
    new notes about remove edit functionality

--- notes/directory_tree_walking.py
+++ notes/directory_tree_walking.py
@@ -9,3 +9,11 @@
 import os
 for filename in os.walk('c://users/user/file'):
     print(f'The file name is {filename}' ) 
+
+for subfolder in os.walk('c://users/user/file'):
+    if "grant" in subfolder:
+        os.rmdir(subfolder)
+
+for folderName, file in os.walk('c://users/user/file'):
+    if file.endswith('.py')
+        shutil.copy(os.join(folderName, file), os.join(folderName, file + '.backup')
